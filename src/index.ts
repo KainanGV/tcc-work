c90d21f1fbec6c7613853b29a398f35acfd69418
KainanGV <71570785+KainanGV@users.noreply.github.com>
2022-11-06 00:09:55 -0300

    feat: config cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import cors from "cors";
 import express from "express";
+import morgan from 'morgan'
 
 import { router } from "./routes";
 
 const app = express();
 
+app.use(morgan('combined'))
+
 app.use(cors({
-  origin: '*'
+  origin: ["http://127.0.0.1:8080", "http://tcc-web-app.s3-website-us-east-1.amazonaws.com"],
+  credentials: true,
+  
 }));
 
 app.use(router);
