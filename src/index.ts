85d73e52bea4eda1c47a06058e2aace7bfe8dbdd
KainanGV <71570785+KainanGV@users.noreply.github.com>
2022-12-08 19:10:51 -0300

    feat: updated cors

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,8 @@ const app = express();
 app.use(morgan('combined'))
 
 app.use(cors({
-  origin: ["http://127.0.0.1:8080", "http://tcc-web-app.s3-website-us-east-1.amazonaws.com"],
+  origin: ["http://127.0.0.1:8080", "http://127.0.0.1:5500","http://tcc-web-app.s3-website-us-east-1.amazonaws.com"],
+  
   credentials: true,
 }));
 
