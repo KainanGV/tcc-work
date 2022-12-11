df5a28458452007a85933eda32d43c3da123e631
KainanGV <kainangabriel2019@gmail.com>
2022-12-11 17:01:07 -0300

    feat: updated cors

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(morgan('combined'))
 
 app.use(cors({
-  origin: ["http://127.0.0.1:8080", "http://127.0.0.1:5500","http://tcc-web-app.s3-website-us-east-1.amazonaws.com"],
+  origin: ["http://127.0.0.1:8080", "http://127.0.0.1:5500","https://tcc-web-app.s3.amazonaws.com"],
   
   credentials: true,
 }));
