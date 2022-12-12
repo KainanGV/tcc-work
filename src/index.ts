bcccdb790d91c24bbc324ba58dd5eff3c53d9c60
KainanGV <kainangabriel2019@gmail.com>
2022-12-12 17:41:22 -0300

    Merge branch 'main' of https://github.com/KainanGV/tcc-work

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
 import cors from "cors";
 import express from "express";
-import morgan from 'morgan'
+import morgan from "morgan";
 
 import { router } from "./routes";
 
 const app = express();
 
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
-app.use(cors({
-  origin: ["http://127.0.0.1:8080", "http://127.0.0.1:5500","https://tcc-web-app.s3.amazonaws.com"],
-  
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "http://127.0.0.1:8080",
+      "http://127.0.0.1:5500",
+      "https://tcc-web-app.s3.amazonaws.com",
+      "http://tcc-web-app.s3-website-us-east-1.amazonaws.com",
+    ],
+
+    credentials: true,
+  }),
+);
 
 app.use(router);
 app.use(express.json());
--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,4 @@ import app from ".";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Aplication is running on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`Aplication is running on PORT ${PORT}.`));
